3e3b94529f18251af379da8fd09bbefa9ccbc0ea
Justine Oyaghiro <justineoyaghiro@icloud.com>
2026-05-26 11:25:41 -0400

    generated drink constructor

--- src/main/java/com/pluralsight/models/Drink.java
+++ src/main/java/com/pluralsight/models/Drink.java
@@ -2,4 +2,9 @@ package com.pluralsight.models;
 
 public class Drink {
     private String size, flavor;
+
+    public Drink(String size, String flavor) {
+        this.size = size;
+        this.flavor = flavor;
+    }
 }

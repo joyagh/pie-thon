c0045cd700e341b2bb72663672fa70d3d34cb669
Justine Oyaghiro <justineoyaghiro@icloud.com>
2026-05-26 10:52:43 -0400

    created toppings constructor

--- src/main/java/com/pluralsight/models/Toppings.java
+++ src/main/java/com/pluralsight/models/Toppings.java
@@ -2,5 +2,12 @@ package com.pluralsight.models;
 
 public class Toppings {
     private String name, category;
+
+    public Toppings(String name, String category, boolean hasExtra) {
+        this.name = name;
+        this.category = category;
+        this.hasExtra = hasExtra;
+    }
+
     private boolean hasExtra;
 }

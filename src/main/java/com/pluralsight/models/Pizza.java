a81a3bfb5d0161b22281a74bae16251cda6e81be
Justine Oyaghiro <justineoyaghiro@icloud.com>
2026-05-26 10:58:57 -0400

    created pizza variables

--- src/main/java/com/pluralsight/models/Pizza.java
+++ src/main/java/com/pluralsight/models/Pizza.java
@@ -1,4 +1,10 @@
 package com.pluralsight.models;
 
+import java.util.List;
+
 public class Pizza {
+    private String size, crust;
+    private boolean hasStuffedCrust;
+    private List<Toppings> toppings;
+
 }

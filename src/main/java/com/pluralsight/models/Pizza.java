7c05c92a4bd5fa6e2c703ffcc708b1e0937d131f
Justine Oyaghiro <justineoyaghiro@icloud.com>
2026-05-26 11:01:18 -0400

    created getters and setters for pizza class

--- src/main/java/com/pluralsight/models/Pizza.java
+++ src/main/java/com/pluralsight/models/Pizza.java
@@ -8,11 +8,43 @@ public class Pizza {
     private boolean hasStuffedCrust;
     private List<Toppings> toppings;
 
+    public String getSize() {
+        return size;
+    }
+
+    public void setSize(String size) {
+        this.size = size;
+    }
+
+    public String getCrust() {
+        return crust;
+    }
+
+    public void setCrust(String crust) {
+        this.crust = crust;
+    }
+
+    public boolean isHasStuffedCrust() {
+        return hasStuffedCrust;
+    }
+
+    public void setHasStuffedCrust(boolean hasStuffedCrust) {
+        this.hasStuffedCrust = hasStuffedCrust;
+    }
+
+    public List<Toppings> getToppings() {
+        return toppings;
+    }
+
+    public void setToppings(List<Toppings> toppings) {
+        this.toppings = toppings;
+    }
+
     public Pizza(String size, String crust, boolean hasStuffedCrust, List<Toppings> toppings) {
         this.size = size;
         this.crust = crust;
         this.hasStuffedCrust = hasStuffedCrust;
         this.toppings = new ArrayList<>();
-        ;
+
     }
 }

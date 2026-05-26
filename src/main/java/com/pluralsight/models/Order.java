85e190797293b13eccf362222eb8240c01d00460
Justine Oyaghiro <justineoyaghiro@icloud.com>
2026-05-26 15:46:41 -0400

    completed order class

--- src/main/java/com/pluralsight/models/Order.java
+++ src/main/java/com/pluralsight/models/Order.java
@@ -1,4 +1,35 @@
 package com.pluralsight.models;
 
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Order {
+    private List<OrderItem> items;
+    private LocalDateTime orderTime;
+
+    public Order() {
+        items = new ArrayList<>();
+        orderTime = LocalDateTime.now();
+    }
+
+    public void addItem(OrderItem item) {
+        items.add(item);
+    }
+
+    public List<OrderItem> getItems() {
+        return items;
+    }
+
+    public LocalDateTime getOrderTime() {
+        return orderTime;
+    }
+
+    public double getTotal() {
+        double total = 0;
+        for (OrderItem item : items) {
+            total += item.getPrice();
+        }
+        return total;
+    }
 }

bd1c3393841f85d073fb470f340abc9c3f7c3e8b
Justine Oyaghiro <justineoyaghiro@icloud.com>
2026-05-26 11:36:04 -0400

    added missing constructor to garlicknot class

--- src/main/java/com/pluralsight/models/GarlicKnots.java
+++ src/main/java/com/pluralsight/models/GarlicKnots.java
@@ -3,6 +3,10 @@ package com.pluralsight.models;
 public class GarlicKnots implements OrderItem{
     private int quantity;
 
+    public GarlicKnots(int quantity) {
+        this.quantity = quantity;
+    }
+
     public int getQuantity() {
         return quantity;
     }

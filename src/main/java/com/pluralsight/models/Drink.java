b87d682b33f3da5063b5394a34506193b098d519
Justine Oyaghiro <justineoyaghiro@icloud.com>
2026-05-26 11:26:18 -0400

    generated drink getters and setters

--- src/main/java/com/pluralsight/models/Drink.java
+++ src/main/java/com/pluralsight/models/Drink.java
@@ -3,6 +3,22 @@ package com.pluralsight.models;
 public class Drink {
     private String size, flavor;
 
+    public String getSize() {
+        return size;
+    }
+
+    public void setSize(String size) {
+        this.size = size;
+    }
+
+    public String getFlavor() {
+        return flavor;
+    }
+
+    public void setFlavor(String flavor) {
+        this.flavor = flavor;
+    }
+
     public Drink(String size, String flavor) {
         this.size = size;
         this.flavor = flavor;

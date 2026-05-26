453d8db4fd26ebf58b449ccd5a4b55d8749bd230
Justine Oyaghiro <justineoyaghiro@icloud.com>
2026-05-26 11:34:18 -0400

    created garlicknot class

--- src/main/java/com/pluralsight/models/GarlicKnots.java
+++ src/main/java/com/pluralsight/models/GarlicKnots.java
@@ -1,4 +1,13 @@
 package com.pluralsight.models;
 
-public class GarlicKnots {
+public class GarlicKnots implements OrderItem{
+    private int quantity;
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public double getPrice() {
+        return 0;
+    }
 }

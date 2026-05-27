15f2b0b2f79b325e95ad3f3b1ade686c2d831897
Justine Oyaghiro <justineoyaghiro@icloud.com>
2026-05-27 13:41:57 -0400

    added drink pricing logic

--- src/main/java/com/pluralsight/models/Drink.java
+++ src/main/java/com/pluralsight/models/Drink.java
@@ -19,6 +19,14 @@ public class Drink implements OrderItem {
 
     @Override
     public double getPrice() {
+        if (size.equalsIgnoreCase("Small")) {
+            return 2.00;
+        } else if (size.equalsIgnoreCase("Medium")) {
+            return 2.50;
+        }else if(size.equalsIgnoreCase("Large")){
+            return 3.00;
+        }
         return 0;
     }
 }
+

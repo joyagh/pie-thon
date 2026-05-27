b7fc69e791d29aa129b59b49c37aab307fb8f528
Justine Oyaghiro <justineoyaghiro@icloud.com>
2026-05-27 08:54:04 -0400

    started main, added scanner and souts for input

--- src/main/java/com/pluralsight/ui/Main.java
+++ src/main/java/com/pluralsight/ui/Main.java
@@ -1,4 +1,21 @@
 package com.pluralsight.ui;
 
+import java.util.Scanner;
+
 public class Main {
+
+    static Scanner input = new Scanner(System.in);
+    public static boolean running = true;
+
+    public static void main(String[] args) {
+
+
+
+        while(running){
+            System.out.println("Welcome to Pie-thon!");
+            System.out.println("1) New Order");
+            System.out.println("0) Exit");
+            System.out.print("Enter Option Here");
+        }
+    }
 }
